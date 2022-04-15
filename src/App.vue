75061336306fa8792dd3386f25ab594dd3c2b99a
ryota-09 <n.ryo950911@gmail.com>
2022-04-16 08:18:47 +0900

    initialized app

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterLink to="/">Home</RouterLink>&nbsp;|
+  <RouterView />
+</template>
+
+<style>
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+  text-align: center;
+  font-weight: normal;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <h1>タイトル</h1>
+</template>
