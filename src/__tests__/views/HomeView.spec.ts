1e35b64fe3da12fd64588f8f7af2c3d41d33cd31
ryota-09 <n.ryo950911@gmail.com>
2022-04-16 08:49:51 +0900

    first test実装

---
+++ src/__tests__/views/HomeView.spec.ts
@@ -0,0 +1,14 @@
+// グローバルファイルとして認識させる必要がある。
+export {};
+//describe("ここにテスト名", () => { テストの処理 })
+describe("HomeView.vueの単体テスト", () => {
+  // test関数またはit関数が使える。
+  test("num2に入る値のテスト", () => {
+    const num1 = 1;
+    const num2 = num1 + 4;
+    // expect( 対象の変数、値のを確認したいもの ).toBe( 期待する値 );
+    expect(num2).toBe(5);
+  });
+});
+// npm run test でテストを動かす。
+//常時動かしたい時は npm run test:watch
