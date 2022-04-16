5c8b40ed4dec7199ec64eb09f2b5755c029d2488
ryota-09 <n.ryo950911@gmail.com>
2022-04-16 13:12:36 +0900

    外部ライブラリーのテスト実装

--- src/__tests__/views/AboutView.spec.ts
+++ src/__tests__/views/AboutView.spec.ts
@@ -1,14 +1,29 @@
 export{};
+import axios from "axios";
+
 describe("AboutView.vueの単体テスト", () => {
   test("Implementation関数のサンプル", () => {
     // モック化
     const myMock = jest.fn();
-    // ダミーの関数を作る。
+    // 任意の戻り値を返すダミーの関数を作る。
     const dummyFunction = myMock.mockImplementation(() => {
       return "ダミーの戻り値を返す。"
     });
     // 省略もできる。dummyFunction = myMock(() => { return "ダミー" })
     // 関数なので()が必要。
     expect(dummyFunction()).toBe("ダミーの戻り値を返す。")
+  });
+
+  // http://demo8969917.mockable.io/jest-mock
+  // 外部ライブラリーをまるっとmockにする。jest.fn()でモック化された状態になる。
+  // ここがビミョー。
+  jest.mock("axios")
+  test("axiosなどの外部のライブラリーを含むメソッドのテスト", async () => {
+    // axiosがモック化されるはずだが。。。
+    const asyncMock = jest.fn();
+    const expectData = asyncMock.mockResolvedValue({ name: "鈴木たろう" });
+    const userList = [{ name: "鈴木たろう" }];
+
+    await expect(expectData()).resolves.toEqual(userList[0]);
   });
 })
