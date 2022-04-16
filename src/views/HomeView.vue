0ad837c5c2480e689d7ea2fc250e4b78ffab6664
ryota-09 <n.ryo950911@gmail.com>
2022-04-16 09:28:23 +0900

    概要の実装

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,5 +3,28 @@
 </script>
 
 <template>
-  <h1>タイトル</h1>
+  <h1>\ Jest Tutorial ! /</h1>
+  <h2>~ Vite, Vue3, TypeScript ~</h2>
+  <br />
+  <hr>
+  <h3>そもそもテストって？</h3>
+  <p>・総合テスト</p>
+  <p>・結合テスト</p>
+  <p>・単体テスト</p>
+  <p> <b>単体テスト</b>でJestを利用する</p>
+  <a href="https://pm-rasinban.com/ut-it-st">引用サイト</a>
+  <hr>
+  <h3>Vueの単体テスト</h3>
+  <p><b>・scriptタグのメソッドのテスト</b></p>
+  <p>基本的に全てのメソッドに関するテスト。ここでJestを使う。</p>
+  <p><b>・templateタグの表示のテスト</b></p>
+  <p>全ての表示に関するテスト。ここでVue公式のテストフレームワークであるVue Test Utilsを使う。</p>
+  <a href="https://jestjs.io/ja/">Jest公式doc</a> &nbsp;
+  <a href="https://v1.test-utils.vuejs.org/guides/#getting-started">VueTestUtils公式doc</a>
+  <hr>
+  <h3>Jestの仕組み</h3>
+  <p>srcフォルダ直下の<b>「__tests__」</b>フォルダ(アンダーバー2つ)の中の</p>
+  <p><b>「◯◯.spec.ts」</b>というファイルの中をテストファイルと認識している。</p>
+  <P>テストファイルは.vueファイルではなく、全て<b>tsファイル</b>ということに注意。</P>
+  <br>
 </template>
