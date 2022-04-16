b91bd0fea65d4fa46dd6478b4c39f68292804277
ryota-09 <n.ryo950911@gmail.com>
2022-04-16 09:42:16 +0900

    notに関するテスト

--- src/__tests__/views/HomeView.spec.ts
+++ src/__tests__/views/HomeView.spec.ts
@@ -9,6 +9,15 @@ describe("HomeView.vueの単体テスト", () => {
     // expect( 対象の変数、値のを確認したいもの ).toBe( 期待する値 );
     expect(num2).toBe(5);
   });
+
+  describe("not関数のサンプル", () => {
+    test("num1に入る値のテスト", () => {
+      const num1 = 1;
+      const num2 = num1 + 4;
+      // 100じゃなかったらテストがPassする。
+      expect(num2).not.toBe(100);
+    })
+  })
 });
 // npm run test でテストを動かす。
 //常時動かしたい時は npm run test:watch
